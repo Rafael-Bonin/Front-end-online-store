4eb7525f09fdaceb686cab783a0b09fafb9bee79
Rafael <rafalyf11@gmail.com>
2022-01-19 18:40:46 -0300

    correcoes lint

--- src/services/api.js
+++ src/services/api.js
@@ -1,20 +1,9 @@
-import React from 'react';
-import query from '../__mocks__/query';
-
-
-export const getCategories = () => 
-  fetch('https://api.mercadolibre.com/sites/MLB/categories')
+export const getCategories = () => fetch('https://api.mercadolibre.com/sites/MLB/categories')
   .then((response) => response.json())
   .then((data) => data)
-  .catch(error => error);
+  .catch((error) => error);
 
-const categories =  getCategories();
-
-export  const getProductsFromCategoryAndQuery = (categoryId, query) => 
-  fetch(`https://api.mercadolibre.com/sites/MLB/search?category=${categoryId}&q=${query}`)
+export const getProductsFromCategoryAndQuery = (categoryId, query) => fetch(`https://api.mercadolibre.com/sites/MLB/search?category=${categoryId}&q=${query}`)
   .then((response) => response.json())
   .then((data) => data)
-  .catch(error => error);
-  
-
-
+  .catch((error) => error);
